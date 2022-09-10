de3e7a9e5f35f427fe4d46e335e72617fe9238d2
pradyaheningpertiwi <112565459+pradyaheningpertiwi@users.noreply.github.com>
2022-09-10 16:47:24 +0700

    Add files via upload

---
+++ input output.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    string nama;
+    string NPM;
+    string jurusan;
+    int lahir;
+    int usia;
+    getline(cin,nama);
+    getline(cin,NPM);
+    getline(cin,jurusan);
+    cin>>lahir;
+   
+    
+    usia=2022-lahir;
+    cout<<endl;
+    
+    cout<<"Nama Saya \""<<nama<<"\","<<endl;
+    cout<<"umur saya \""<<usia<<" Tahun\","<<endl;
+    cout<<"dengan NPM \""<<NPM<<"\","<<endl;
+    cout<<"dari jurusan "<<jurusan;
+    
+    return 0;
+    
+    
+    
+
+ 
+	
+    
+    
+    
+    
+    
+        
+
+}
+
